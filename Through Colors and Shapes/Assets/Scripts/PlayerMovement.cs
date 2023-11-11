60821b1f2a35c5c739a81350ce79691c7546e2cd
DerpyMissile <hmai015@ucr.edu>
2023-11-11 11:50:48 -0800

    BOOYAH

--- Through Colors and Shapes/Assets/Scripts/PlayerMovement.cs
+++ Through Colors and Shapes/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed;
+    public float Move;
+    public float jump;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(speed * Move, rb.velocity.y);
+        if(Input.GetAxis("Vertical") > 0){
+            rb.AddForce(new Vector2(rb.velocity.x, jump));
+        }else{
+            rb.AddForce(new Vector2(rb.velocity.x, -jump));
+        }
     }
 }
