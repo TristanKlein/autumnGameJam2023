97a0d925e85871d6ec26f7c645cf034740c2a4f5
TristanKlein <tristanepix@gmail.com>
2023-11-11 16:06:34 -0800

    Revert "Revert "guh""
    This reverts commit bf24bc6f33f3d3b402b7adc4119679e500695ab0.

---
+++ Through Colors and Shapes/Assets/Scripts/Finish_Line.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Finish_Line : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void LoadNextScene()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        int nextIndex = currentScene + 1;
+        if (nextIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextIndex);
+        }
+        else
+        {
+            SceneManager.LoadScene(0);
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            LoadNextScene();
+        }
+    }
+}
+
+
